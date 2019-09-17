ad3a90cfbed450dcc87105370331b3f85ff7b970
Gerard Dethier <g.dethier@gmail.com>
2019-09-17 16:35:59 +0200

    Add maven plugin site.

---
+++ _scripts/sync_maven_plugin_doc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PLUGIN_DOC_PATH="pousse-cafe-maven-plugin"
+
+if [ ! -d $PLUGIN_DOC_PATH ]; then
+    echo "Destination directory does not exist"
+    exit 1
+fi
+
+rm -rf $PLUGIN_DOC_PATH/*
+( cd ../pousse-cafe/pousse-cafe-maven-plugin ; mvn clean site )
+cp -r ../pousse-cafe/pousse-cafe-maven-plugin/target/site/* $PLUGIN_DOC_PATH
