f653caa33bd0d77132b3311b02d6340adab36a23
Gerard Dethier <g.dethier@gmail.com>
2021-01-19 17:20:33 +0100

    feat: update shop-app doc.

--- _scripts/sync_shop_doc.sh
+++ _scripts/sync_shop_doc.sh
@@ -4,5 +4,8 @@
 cd ../pousse-cafe-shop-app/ ;
 mvn pousse-cafe:generate-doc -DbasePackage=poussecafe.shop -DdomainName=Shop -Dversion=Latest
 )
+
+rm -f shop-doc/pousse-cafe-shop-app-*.pdf
 cp -rf ../pousse-cafe-shop-app/target/ddd-doc/* shop-doc/
 mv shop-doc/pousse-cafe-shop-app-*.pdf shop-doc/pousse-cafe-shop-app-Latest.pdf
+
